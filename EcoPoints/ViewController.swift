1d2023f37c04f6aed5b449166775a855e9eed985
Marie Ow <marieow@maries-macbook-pro.local>
2018-12-26 23:25:16 -0500

    Initial Commit

---
+++ EcoPoints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoPoints
+//
+//  Created by Marie Ow on 12/26/18.
+//  Copyright © 2018 Marie Ow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
